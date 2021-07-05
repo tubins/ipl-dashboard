72813f5c46f32899da46e7d65b6715772a50787b
tubins <tubin.shaji@gmail.com>
2021-07-05 23:38:05 +0100

    Add sprigboot project for backend

---
+++ ipl-be/src/main/java/com/tubz/iplbe/IplbeApplication.java
@@ -0,0 +1,13 @@
+package com.tubz.iplbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplbeApplication.class, args);
+	}
+
+}
